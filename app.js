447323a571dc4e8eec1f35ac2f4b14885d2a5c95
sett <kguswls2684@naver.com>
2025-10-30 20:05:15 +0900

    app.js에 웹소켓

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 const app = express()
 const path = require('path')
+const websocket = require('ws')
+const http = require('http')
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
@@ -28,6 +30,30 @@ app.get('/', (req, res)=>{
     res.redirect('/main')
 })
 
-app.listen(80, ()=>{
-    console.log('app 80 서버 확인')
+// 1 express 기반 http 서버 생성
+const server = http.createServer(app)
+// 2 WebSocket 서버 생성
+const wss = new websocket.Server({server})
+// 4 클라이언트 접속
+wss.on('connection', (ws, req)=>{
+    // 5.1 클라이언트로부터 메세지 수신
+    ws.on('message', (msg)=>{
+        console.log(`클라이언트로부터 받은 메세지 : ${msg}`)
+    })
+    // 5.2 서버가 메세지 수신
+    ws.send('서버가 메세지 보냄')
+
+    ws.on('err', (err)=>{
+        console.log(`에러발생 : ${err.message}`)
+    })
+
+    ws.on('close', ()=>{
+        console.log(`클라이언트 연결 종료`)
+    })
+})
+
+
+// 3 서버 실행
+server.listen(80, ()=>{
+    console.log('웹소켓 server 80 서버 확인')
 })
