d1eb1f34f1ef1f643c70bfe006eacb71fab0205d
sett <kguswls2684@naver.com>
2025-10-30 16:50:11 +0900

    reserve_aside

--- routes/desRouter.js
+++ routes/desRouter.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const conn = require('../db/db')
 const router = express.Router()
 const app = express()
 
@@ -20,15 +21,28 @@ router.get('/reserve', (req, res)=>{
     res.render("reserve.html", arr)
 })
 
+
 router.post('/temp', (req, res)=>{
     const {dd, tt, flag} = req.body
     console.log(dd, tt)
-    arr = {
-        date: dd,
-        time: tt,
-        flag: flag
-    }
-    console.log(arr)
+    
+    
+    conn.query('select * from theater_info where id = "1"', (err, resQuery)=>{
+        if(err) {
+            console.log('sql 실패', err.message)
+            res.render('../views/list.html')
+        } else {
+            console.log('sql 성공', resQuery)
+            // res.render('../views/list.html', {res : resQuery})
+            arr = {
+                date: dd,
+                time: tt,
+                flag: flag,
+                res: resQuery
+            }
+        }
+        console.log(arr)
+    })
     
     res.json({message: 'receive'})
 })
