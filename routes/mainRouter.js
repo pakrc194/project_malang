a2daba338ab19d86be36ca8ed7a7a2899f21a73a
이광재 <sck09013@naver.com>
2025-11-05 20:00:33 +0900

    2025.11.05 노민호님꺼 도둑질

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -16,5 +16,36 @@ router.get('/', (req, res) => {
 
 })
 
+router.get('/', (req, res)=>{
+    conn.query('select * from performance_info', (err, resQuery)=>{
+        if(err) {
+            console.log('테이블 조회 실패', err.message)
+            res.render('../views/main.html')
+        } else {
+            console.log('테이블 조회 성공', resQuery)
+            res.render('../views/main.html', {res : resQuery})
+        }
+    })
+
+
+    // res.render('../views/main.html')
+})
+
+
+router.get('/grade', (req, res)=>{
+    conn.query('select * from user_grade', (err, resQuery)=>{
+        if(err) {
+            console.log('등급 조회 실패', err.message)
+
+            res.render('../views/grade.html')
+        } else {
+            console.log('등급 조회 성공', resQuery)
+            console.log('등급 조회 성공', resQuery[0])
+            console.log('등급 조회 성공', resQuery[0].grade_score)
+            res.render('../views/grade.html', {grade_list : resQuery})
+        }
+    })
+})
+
 
 module.exports = router
